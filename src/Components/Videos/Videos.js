6bbe92285beae6a7f7d4b98b72f61358f0bcd574
Michael Alfandre <Michael Alfandre>
2019-03-25 16:14:26 -0600

    before pull

---
+++ src/Components/Videos/Videos.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, Suspense, memo } from 'react'
+import './BuyBox.scss'
+
+const Videos = () => {
+
+    return (
+
+        <div className="buybox">
+            SpikeBall
+        </div>
+    )
+}
+
+export default memo(Videos)
