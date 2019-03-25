0269008aee06b17bec781175391e1a6f613bed22
Michael Alfandre <Michael Alfandre>
2019-03-25 10:31:57 -0600

    completed 5 unit tests for authentication

--- src/Testing/app.test.js
+++ src/Testing/app.test.js
@@ -1,7 +1,31 @@
-describe('Login', () => {
+import {validateEmail, validatePassword} from './AuthLogic'
 
-    test('is not case sensitive', () => {
-        let result = update();
+describe('Valid emails', () => {
+
+    test('Should be true', () => {
+        let result = validateEmail('m.a@yahoo.com');
         expect(result).toBeTruthy();
     })
+    test('Should be false', () => {
+        let result = validateEmail('michael');
+        expect(result).toBeFalsy();
+    })
+    test('Should be false', () => {
+        let result = validateEmail('joe@gmail');
+        expect(result).toBeFalsy();
+    })
 })
+
+describe('Valid Password', () => {
+
+    test('Please enter a password', () => {
+        let result = validatePassword('');
+        expect(result).toEqual({ "bool": false, "message": "Please enter a password" });
+    })
+    test('Password length needs to be at least 7 characters', () => {
+        let result = validatePassword('$');
+        expect(result).toEqual({ "bool": false, "message": "Password length needs to be at least 7 characters" });
+    })
+    
+})
+
