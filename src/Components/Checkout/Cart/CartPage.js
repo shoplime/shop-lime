4c164311f20851e7892838ec097ff2519a1dba44
Tyler Simmons <tylersimmons212@gmail.com>
2019-04-02 09:18:12 -0600

    updated cart page

---
+++ src/Components/Checkout/Cart/CartPage.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+
+import CartHeader from './CartHeader';
+import CartItems from './CartItems';
+import CartPageCheckout from '../Checkout/CartPageCheckout'
+import '../Checkout/Checkout.scss'
+
+import { GetProducts } from '../../../ducks/products';
+import { GetCartItems } from '../../../ducks/cart';
+
+class Cart extends Component{
+    
+    componentDidMount() {
+        this.props.GetProducts();
+        this.props.GetCartItems();
+    }
+
+    render(){
+        const { cart, products } = this.props;
+        const { toggleComplete, toggleCheckout, openCheckout } = this.props;
+
+        if (
+            cart.fetched === true &&
+            cart.fetching === false &&
+            products.fetched === true
+            ){
+                if (cart.cart.data[0]) {
+                    var subtotal = '$' + cart.cart.meta.display_price.with_tax.amount / 100;
+                    return(
+                        <div className='cart'>
+                            <CartHeader/>
+                            <div className='cart-header'>
+                                <div id='cart-header-1'>Product</div>
+                                <div id='cart-header-2'>Quantity</div>
+                                <div>Price</div>
+                            </div>
+                            <div className='cart-items'>
+                                <CartItems/>
+                            </div>
+                            <div className='cart-subtotal'>
+                                {'Subtotal of all products '}
+                                <span>{subtotal}</span>
+                            </div>
+                            <div className='CheckoutForm'>
+                                <CartPageCheckout toggleComplete={toggleComplete} toggleCheckout={toggleCheckout} openCheckout={openCheckout}/>
+                            </div>
+                        </div>
+                    )
+                }
+                else{
+                return(
+                    <div>
+                        {/* <CartHeader/> */}
+                        <div>
+                            <p>Look's like your cart is empty!</p>
+                            <Link to='/'>Start Shoping</Link>
+                        </div>
+                    </div>)
+                }
+            } else{
+                return(
+                    <div>
+                        <CartHeader/>
+                        <p>Loading...</p>
+                    </div>
+                )
+            }
+
+    }
+}
+
+const mapStateToProps = ({ products, cart }) => ({
+    products,
+    cart
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      GetProducts,
+      GetCartItems
+    },
+    dispatch
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
---
+++ src/Components/Checkout/Checkout/CartPageCheckout.js
@@ -0,0 +1,263 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as api from '../../../moltin';
+// import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
+import { SUBMIT_PAYMENT, PAYMENT_COMPLETE } from '../../../ducks/payments';
+
+// Material-UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+// Import Components
+// import CheckoutSummary from './CheckoutSummary';
+import Details from './stepper/Details';
+import Billing from './stepper/Billing';
+import Shipping from './stepper/Shipping';
+import Payment from './stepper/Payment';
+import Complete from './stepper/Complete'
+
+function mapStateToProps(state) {
+  return { push: state.push };
+}
+
+var CheckoutTemplate = {
+  customer: {
+    name: 'John Doe',
+    email: 'john@doe.co'
+  },
+  shipping_address: {
+    first_name: 'John',
+    last_name: 'Doe',
+    line_1: '2nd Floor British India House',
+    line_2: '15 Carliol Square',
+    city: 'Newcastle Upon Tyne',
+    postcode: 'NE1 6UF',
+    county: 'Tyne & Wear',
+    country: 'United Kingdom'
+  },
+  billing_address: {
+    first_name: 'John',
+    last_name: 'Doe',
+    line_1: '2nd Floor British India House',
+    line_2: '15 Carliol Square',
+    city: 'Newcastle Upon Tyne',
+    postcode: 'NE1 6UF',
+    county: 'Tyne & Wear',
+    country: 'United Kingdom'
+  }
+};
+var PaymentTemplate = {
+  gateway: 'stripe',
+  method: 'purchase',
+  first_name: 'John',
+  last_name: 'Doe',
+  number: '4242424242424242',
+  month: '08',
+  year: '2020',
+  verification_value: '123'
+};
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  backButton: {
+    marginRight: theme.spacing.unit,
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
+});
+
+function getSteps() {
+  return ['Your Details', 'Billing', 'Shipping', 'Payment'];
+}
+
+
+class CheckoutForm extends Component {
+
+  state = {
+    activeStep: 0
+  };
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return <Details className={this.props.classes.instructions} handleSubmit={this.props.handleSubmit(this.mySubmit)} handleKeyDown={e => this.handleKeyDown(e)} />;
+      case 1:
+        return <Billing className={this.props.classes.instructions} handleSubmit={this.props.handleSubmit(this.mySubmit)} handleKeyDown={e => this.handleKeyDown(e)} />;
+      case 2:
+        return <Shipping className={this.props.classes.instructions} handleSubmit={this.props.handleSubmit(this.mySubmit)} handleKeyDown={e => this.handleKeyDown(e)} />;
+      case 3:
+        return <Payment className={this.props.classes.instructions} handleSubmit={this.props.handleSubmit(this.mySubmit)} handleKeyDown={e => this.handleKeyDown(e)} />;
+      default:
+        return 'Unknown stepIndex';
+    }
+  }
+
+  handleKeyDown = function (e) {
+    if (e.key === 'Enter' && e.shiftKey === false) {
+      e.preventDefault();
+    }
+  };
+
+  mySubmit = values => {
+    CheckoutTemplate.customer.name = values.name;
+    CheckoutTemplate.customer.email = values.email;
+
+    CheckoutTemplate.billing_address.first_name = values.billing_firstname;
+    CheckoutTemplate.billing_address.last_name = values.billing_lastname;
+    CheckoutTemplate.billing_address.line_1 = values.billing_address_1;
+    CheckoutTemplate.billing_address.line_2 = values.billing_address_2;
+    CheckoutTemplate.billing_address.city = values.billing_state;
+    CheckoutTemplate.billing_address.county = values.billing_postcode;
+    CheckoutTemplate.billing_address.country = values.billing_country;
+
+    CheckoutTemplate.shipping_address.first_name = values.shipping_firstname;
+    CheckoutTemplate.shipping_address.last_name = values.shipping_lastname;
+    CheckoutTemplate.shipping_address.line_1 = values.shipping_address_1;
+    CheckoutTemplate.shipping_address.line_2 = values.shipping_address_2;
+    CheckoutTemplate.shipping_address.city = values.shipping_state;
+    CheckoutTemplate.shipping_address.county = values.shipping_postcode;
+    CheckoutTemplate.shipping_address.country = values.shipping_country;
+
+    PaymentTemplate.first_name = values.card_first_name;
+    PaymentTemplate.last_name = values.card_last_name;
+    PaymentTemplate.number = values.number;
+    PaymentTemplate.month = values.month;
+    PaymentTemplate.year = values.year;
+    PaymentTemplate.verification_value = values.card_cvc;
+    // console.log(CheckoutTemplate, PaymentTemplate)
+
+    this.props.dispatch(dispatch => {
+      dispatch({ type: SUBMIT_PAYMENT });
+    });
+
+    api
+      .Checkout(CheckoutTemplate.customer, CheckoutTemplate.billing_address, CheckoutTemplate.shipping_address)
+
+      .then(order => {
+        // console.log(order)
+        api.OrderPay(order.data.id, PaymentTemplate);
+        api.DeleteCart();
+      })
+
+      .then(() => {
+
+        this.props.dispatch(dispatch => {
+          dispatch({ type: PAYMENT_COMPLETE });
+          // dispatch(push('/order-confirmation'));
+        });
+      })
+
+      .catch(e => {
+        console.log(e);
+      })
+
+      .catch(e => {
+        console.log(e);
+      })
+
+      .catch(e => {
+        console.log(e);
+      })
+      this.handleReset()
+    //   this.props.toggleComplete()
+  };
+
+  handleNext = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep + 1,
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1,
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+    const { toggleCheckout, openCheckout } = this.props;
+    return (
+      <main role="main" id="container" className="main-container push">
+        <section className="checkout">
+          <div className="content">
+            {/* <CheckoutSummary /> */}
+
+              <div className={classes.root}>
+
+                <Stepper activeStep={activeStep} alternativeLabel>
+                  {steps.map(label => (
+                    <Step key={label}>
+
+                      <StepLabel>{label}</StepLabel>
+                    </Step>
+                  ))}
+                </Stepper>
+                <div>
+
+                  {this.state.activeStep === steps.length ? (
+                    <div>
+
+                      <Complete />
+                      <Button onClick={this.handleReset}>Reset</Button>
+                    </div>
+                  ) : (
+                      <div>
+
+                        <Typography component={'span'} className='cart-content'>{this.getStepContent(activeStep)}</Typography>
+                        <div className='form-buttons'>
+                          {activeStep === 0?null:<Button
+                            disabled={activeStep === 0}
+                            onClick={this.handleBack}
+                            className={classes.backButton}
+                          >
+                            Back
+                          </Button>}
+                          {activeStep === steps.length - 1 ? <Button variant="contained" className='pay' onClick={this.props.handleSubmit(this.mySubmit)} type="submit" className="pay" color='primary' aria-live="polite">
+                          Pay
+                          </Button>:
+                          <Button variant="contained" id='next' onClick={this.handleNext}>
+                            Next
+                          </Button>}
+                        </div>
+                      </div>
+                    )}
+                </div>
+              </div>
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+CheckoutForm = reduxForm({
+  form: 'CheckoutForm'
+})(CheckoutForm);
+
+
+const CheckoutStepper = connect(mapStateToProps)(CheckoutForm);
+
+CheckoutForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CheckoutStepper);
